59ecae6dc2550fdb6e40fe92e48f8336401caa6c
Maxime Chaillou <maximechaillou@gmail.com>
2026-04-20 13:48:18 +0200

    Update appcast and Cask for v1.0.9

--- Casks/maurice.rb
+++ Casks/maurice.rb
@@ -1,6 +1,6 @@
 cask "maurice" do
-  version "1.0.8"
-  sha256 "ab783cd55ec89a82c406487432786af2a246b797029dc656d1d4048aec0a3358"
+  version "1.0.9"
+  sha256 "f18da69a9ed98821093a6fb065c148a829956911899edba540de88e159e4750c"
 
   url "https://github.com/MaximeChaillou/Maurice/releases/download/v#{version}/Maurice-#{version}.zip"
   name "Maurice"
